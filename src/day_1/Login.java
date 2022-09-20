6ce2f61964900d8cc5b40a8b27e15e8190c3bd13
Mehmet Baran <59487546+baranmmm@users.noreply.github.com>
2022-09-20 20:06:59 +0100

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         //The users can login successfully..
         //Added from github...
+        //use git fetch ana git merge
     }
 }
