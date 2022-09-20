b74f782fccebd14759ec94a121497803f6062c7b
baranmmm <m.baran84@gmail.com>
2022-09-20 21:32:01 +0100

    update CreateChannel

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,6 +2,12 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
-
+        int num1=3;
+        int num2=8;
+if(num1<num2){
+    System.out.println("true");
+}else{
+    System.out.println("false");
+}
     }
 }
