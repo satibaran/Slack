2d857edcfb91031a7ecc8c9f0dd7e9d5b45ff3cc
Mehmet Baran <59487546+baranmmm@users.noreply.github.com>
2022-09-20 22:02:51 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,6 +2,14 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
+        String s="string";
+        if (s.contains("string") {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        
+        
 
     }
 }
